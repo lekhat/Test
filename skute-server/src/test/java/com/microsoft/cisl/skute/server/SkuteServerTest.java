545d2bd1b1abf913d6460383c13a10ca520eb312
Jakob Homan <jghoman@gmail.com>
2014-10-07 15:31:06 -0700

    Add SkuteServerTest for delete.

--- skute-server/src/test/java/com/microsoft/cisl/skute/server/SkuteServerTest.java
+++ skute-server/src/test/java/com/microsoft/cisl/skute/server/SkuteServerTest.java
@@ -6,10 +6,18 @@ import com.microsoft.cisl.skute.filesystem.SkuteFileSystem;
 import com.microsoft.cisl.skute.filesystem.SkuteResult;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.mockito.Mockito.*;
@@ -24,6 +32,14 @@ public class SkuteServerTest {
     LogManager.getLogger("com.microsoft.cisl.skute").setLevel(Level.TRACE);
   }
 
+  private SkuteHttpServer getSkuteHttpServer(SkuteFileSystem sfs) throws Exception {
+    return new SkuteHttpServerBuilder()
+          .setPort(SkuteServer.DEFAULT_PORT)
+          .setServerPackage(SkuteServer.class.getPackage())
+          .setSkuteFileSystem(sfs)
+          .build();
+  }
+
   @Test
   public void mkdirSucceedsWhenExpected() throws Exception {
     SkuteFileSystem sfs = when(mock(SkuteFileSystem.class)
@@ -32,11 +48,7 @@ public class SkuteServerTest {
         .getMock();
 
 
-    SkuteHttpServer shs = new SkuteHttpServerBuilder()
-        .setPort(SkuteServer.DEFAULT_PORT)
-        .setServerPackage(SkuteServer.class.getPackage())
-        .setSkuteFileSystem(sfs)
-        .build();
+    SkuteHttpServer shs = getSkuteHttpServer(sfs);
 
     LOG.info("SkuteHttpServer = " + shs);
 
@@ -55,11 +67,51 @@ public class SkuteServerTest {
     verify(sfs).mkdir("/howdy", (short) 0755);
   }
 
-//  @Test
-//  @Ignore
-//  public void cheat() throws URISyntaxException, IOException {
-//    FileSystem fs = FileSystem.get(new URI("webhdfs://localhost:8000"), new Configuration());
-//
-//    fs.mkdirs(new Path("/howdy"));
-//  }
+  @Test
+  public void deleteRecursiveSucceedsWhenExpected() throws Exception {
+    testSuccessfulDelete(true);
+  }
+
+  @Test
+  public void deleteNonRecursiveSucceedsWhenExpected() throws Exception {
+    testSuccessfulDelete(false);
+  }
+
+  private void testSuccessfulDelete(boolean recursive) throws Exception {
+    final String PATH = "/deleteme";
+    SkuteFileSystem sfs = when(mock(SkuteFileSystem.class)
+        .delete(PATH, recursive))
+        .thenReturn(SkuteResult.OK)
+        .getMock();
+
+
+    SkuteHttpServer shs = getSkuteHttpServer(sfs);
+
+    LOG.info("SkuteHttpServer = " + shs);
+
+    shs.start();
+
+    try {
+      given()
+          .port(SkuteServer.DEFAULT_PORT)
+          .param("op", "delete")
+          .param("recursive", recursive ? "true" : "false")
+          .delete(PREFIX + PATH)
+          .then()
+          .statusCode(200);
+    } finally {
+      shs.stop();
+    }
+    verify(sfs).delete(PATH, recursive);
+  }
+
+  // When run against a local web server, this is a very good quick way of seeing
+  // exactly what WebHDFSFileSystem is requesting...
+  @Test
+  @Ignore
+  public void cheat() throws URISyntaxException, IOException {
+    FileSystem fs = FileSystem.get(new URI("webhdfs://localhost:8000"), new Configuration());
+
+    fs.delete(new Path("/howdy"), true);
+  }
 }
